72484ac2ad91904045fca53c33089946b8449d11
Isaiah Anthony <isaiah.anthony99@gmail.com>
2021-01-16 22:00:53 -0800

    this should work

--- public/assets/burger.js
+++ public/assets/burger.js
@@ -1,21 +1,21 @@
 
-document.getElementById('addUser').addEventListener('click', event => {
+document.getElementById('addBurger').addEventListener('click', event => {
   event.preventDefault()
   axios.post('/api/burgers', {
-    name: document.getElementById('name').value,
-    isHere: false
+    burgerName: document.getElementById('name').value,
+    devoured: false
   })
     .then(() => location.reload())
     .catch(err => console.error(err))
 })
 
 document.addEventListener('click', event => {
-  if (event.target.className === 'isHere') {
-    axios.put(`/api/users/${event.target.dataset.id}`, { isHere: true })
+  if (event.target.className === 'devoured') {
+    axios.put(`/api/burgers/${event.target.dataset.id}`, { devoured: true })
     .then(() => location.reload())
     .catch(err => console.error(err))
   } else if (event.target.className === 'delete') {
-    axios.delete(`/api/users/${event.target.dataset.id}`)
+    axios.delete(`/api/burgers/${event.target.dataset.id}`)
     .then(() => location.reload())
     .catch(err => console.error(err))
   }
