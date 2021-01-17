9885ae9b3a76de689ef58625c29c7ad93dcd3096
Isaiah Anthony <isaiah.anthony99@gmail.com>
2021-01-16 16:05:14 -0800

    all da code

---
+++ db/seed.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+INSERT INTO burgers (burgerName, devoured)
+VALUES ('hamburger', false), 
+('cheeseburger', false), 
+('bacon cheeseburger', false)
+ 
---
+++ models/burger.js
@@ -0,0 +1,18 @@
+const orm = require('../config/orm.js')
+
+const burger = {
+  getAll (cb) {
+    orm.getAll('burgers', burgers => cb(burgers))
+  },
+  create (user, cb) {
+    orm.create('burgers', burger, () => cb())
+  },
+  update (changes, id, cb) {
+    orm.update('burgers', changes, { id }, () => cb())
+  },
+  delete (id, cb) {
+    orm.delete('burgers', { id }, () => cb())
+  }
+}
+
+module.exports = burger
