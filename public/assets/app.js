f4324482a7128a670bca559363bfdfd9b210dc62
Isaiah Anthony <isaiah.anthony99@gmail.com>
2021-01-16 17:32:00 -0800

    contains, not ===

--- public/assets/app.js
+++ public/assets/app.js
@@ -10,7 +10,7 @@ document.getElementById('addBurger').addEventListener('click', event => {
 })
 
 document.addEventListener('click', event => {
-  if (event.target.className === 'devoured') {
+  if (event.target.className.contains('devoured')) {
     axios.put(`/api/burgers/${event.target.dataset.id}`, { devoured: true })
     .then(() => location.reload())
     .catch(err => console.error(err))
